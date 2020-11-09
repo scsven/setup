cb1deb55012d416cd4a24b7c5b3776851ab1e216
scsven <wxyucs@gmail.com>
2020-11-09 14:10:30 +0800

    change ~ to /Users/scsven
    Signed-off-by: scsven <wxyucs@gmail.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,14 +7,14 @@ sudo apt install -y openssh-server vim g++ gcc htop zsh
 echo "Y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 # Download .vimrc_vundle and install
-wget -O ~/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
+wget -O $HOME/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
 vim +PluginInstall +qall
-rm ~/.vimrc
+rm $HOME/.vimrc
 
 # Download .vimrc
-wget -O ~/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc
-wget -O ~/.vimrc_vundle https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
+wget -O $HOME/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc
+wget -O $HOME/.vimrc_vundle https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
 
