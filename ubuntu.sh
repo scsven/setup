a7bbcad1f6aecba0f0c5403b4dcee9011d572116
scsven <wxyucs@gmail.com>
2020-11-09 14:06:45 +0800

    vim vundle install
    Signed-off-by: scsven <wxyucs@gmail.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,8 +9,12 @@ echo "Y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/too
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Download .vimrc_vundle and install
+wget -O ~/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
+vim +PluginInstall +qall
+rm ~/.vimrc
+
 # Download .vimrc
 wget -O ~/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc
+wget -O ~/.vimrc_vundle https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
 
-# vim Vundle install
-vim +PluginInstall +qall
