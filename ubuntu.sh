1dee90d2eb2d7ddbc16ccae9163e1a51931fe4ef
Wang Xiangyu <xy.wang@zilliz.com>
2020-11-09 13:07:50 +0800

    auto change default shell to zsh
    Signed-off-by: Wang Xiangyu <xy.wang@zilliz.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt install -y openssh-server vim g++ gcc htop zsh
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
