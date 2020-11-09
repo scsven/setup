61002b47290d142285392a68f7a28b3f938e72bc
Wang Xiangyu <xy.wang@zilliz.com>
2020-11-09 13:12:12 +0800

    add vimrc
    Signed-off-by: Wang Xiangyu <xy.wang@zilliz.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,3 +9,5 @@ echo "Y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/too
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Download .vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc
