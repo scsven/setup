418be3195a5fe600bf8adff83eeec093c60a588c
Wang Xiangyu <xy.wang@zilliz.com>
2020-11-09 13:10:48 +0800

    add Vundle
    Signed-off-by: Wang Xiangyu <xy.wang@zilliz.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,3 +5,7 @@ sudo apt install -y openssh-server vim g++ gcc htop zsh
 
 # Install oh-my-zsh
 echo "Y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
