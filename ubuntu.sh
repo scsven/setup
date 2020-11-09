646134574ec601954ea2098e6ef6c0c3446e0cca
scsven <wxyucs@gmail.com>
2020-11-09 15:03:26 +0800

    install oh-my-zsh manually
    Signed-off-by: scsven <wxyucs@gmail.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,9 +3,6 @@
 sudo apt update
 sudo apt install -y openssh-server vim g++ gcc htop zsh
 
-# Install oh-my-zsh
-echo "Y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
@@ -17,4 +14,10 @@ rm $HOME/.vimrc
 # Download .vimrc
 wget -O $HOME/.vimrc https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc
 wget -O $HOME/.vimrc_vundle https://raw.githubusercontent.com/scsven/vimrc/master/.vimrc_vundle
+
+
+# Install manually
+
+# oh-my-zsh
+wget -O $HOME/oh-my-zsh-install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 
